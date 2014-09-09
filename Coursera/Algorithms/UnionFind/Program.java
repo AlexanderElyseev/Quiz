b2628344b656cea7f1b70f041723fd9b6ca8447c
Alexander Elyseev <alexander.elyseev@gmail.com>
2014-09-09 22:57:44 +0600

    Coursera "Algorithms I".
    UnionFind Java dummy implementation.

---
+++ Coursera/Algorithms/UnionFind/Program.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Program
+{
+    /**
+     * Program main entry point.
+     *
+     * @param args Command line arguments.
+     */
+	public static void main(String[] args) throws Exception {
+		if (args.length == 0)
+		{
+			System.out.println("Data file is not specified.");
+			return;
+		}
+
+		String fileName = args[0];
+		System.out.println("Reading file \"" + fileName + "\"...");
+
+        UnionFind uf = readInputFile(fileName);
+
+        System.out.println("Data is loaded.");
+	}
+
+    /**
+     * Reads the file from specified path and build UnionFind structure.
+     *
+     * If line contains two numbers ("10 20"), that elements are connected.
+     * Other lines are skipped.
+     *
+     * @param fileName The name of file with data.
+     * @return UnionFind structure with data from the file.
+     */
+    private static UnionFind readInputFile(String fileName) throws IOException {
+        UnionFind uf = new UnionFind();
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        try {
+            String line = br.readLine();
+            Pattern pattern = Pattern.compile("-?\\d+");
+            int[] numbers = new int[2];
+            while (line != null) {
+                int index = 0;
+                Matcher matcher = pattern.matcher(line);
+                while (matcher.find()) {
+                    numbers[index++] = Integer.parseInt(matcher.group());
+                }
+
+                line = br.readLine();
+
+                if (index != 2)
+                    continue;
+
+                uf.union(numbers[0], numbers[1]);
+            }
+        } finally {
+            br.close();
+        }
+        return uf;
+    }
+}
---
+++ Coursera/Algorithms/UnionFind/UnionFind.java
@@ -0,0 +1,16 @@
+class UnionFind
+{
+	public UnionFind()
+	{
+	}
+
+	public void union(int p, int q)
+	{
+
+	}
+
+	public boolean isConnected(int p, int q)
+	{
+		return false;
+	}
+}
