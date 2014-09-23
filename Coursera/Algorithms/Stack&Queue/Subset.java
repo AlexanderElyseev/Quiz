96a705057cfd97dae0d2c245b5ffa4edc0df5094
Alexander Elyseev <alexander.elyseev@gmail.com>
2014-09-23 23:50:57 +0600

    Reading input performance improved.

--- Coursera/Algorithms/Stack&Queue/Subset.java
+++ Coursera/Algorithms/Stack&Queue/Subset.java
@@ -4,12 +4,11 @@ public class Subset {
     public static void main(String[] args) {
         // Reading input.
         int count = Integer.parseInt(args[0]);
-        String input = StdIn.readLine();
 
         // Initializing queue.
         RandomizedQueue<String> queue = new RandomizedQueue<String>();
-        for (String item : input.split("\\s+"))
-            queue.enqueue(item);
+        while (!StdIn.isEmpty())
+            queue.enqueue(StdIn.readString());
 
         // Printing output.
         Iterator<String> it = queue.iterator();
